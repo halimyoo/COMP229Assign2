9669d0ed75021ca3c90a2c6b5a3bd28f92324cb7
HalimYoo <ziovs02@gmail.com>
2021-10-23 23:13:09 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports =
+{
+    //"URI" : "mongodb://localhost/book_store" 
+    "URI" : "mongodb+srv://Halim:BCDNEJK02EO1ebw6@mongodbserver.oplwz.mongodb.net/book_store?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
